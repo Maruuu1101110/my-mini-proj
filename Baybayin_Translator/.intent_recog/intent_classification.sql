9d301eb93a2c5af180a894a2e90fd14c9f1db433
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-04-27 02:06:17 +0800

    removed some hidden files

--- Baybayin_Translator/.intent_recog/intent_classification.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE intents (
-    intent VARCHAR PRIMARY KEY,
-    keywords JSON
-);
--- Baybayin_Translator/.intent_recog/transformers_tryrun.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="microsoft/DialoGPT-small")
-
-response = generator("Hello, how are you?", max_length=50)
-print(response[0]['generated_text'])
